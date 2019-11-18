66d87e70a8b9b342a4c2be577920416f2103d17a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-18 18:43:16 +0100

    Added graph and details to begin at room 0

---
+++ room_map/room_details.py
@@ -0,0 +1,1 @@
+[{"room_id": 0, "title": "A Dark Room", "description": "You cannot see anything.", "coordinates": "(60,60)", "exits": ["n", "s", "e", "w"], "cooldown": 1.0, "errors": [], "messages": []}]
